e1007513118e56dfad9955a3198f613fa562c632
Robert-Kilduff <68850051+Robert-Kilduff@users.noreply.github.com>
2024-03-05 19:40:31 +0100

    added single thread server files.
    Added single thread server files.

---
+++ single_thread_server/src/main.rs
@@ -0,0 +1,18 @@
+mod server;
+use server::Server;
+fn main() {
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+
+
+
+
+
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
