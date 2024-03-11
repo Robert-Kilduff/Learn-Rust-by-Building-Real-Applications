b7873fd82972852d58b524ad14f21b95751f83fd
Robert-Kilduff <68850051+Robert-Kilduff@users.noreply.github.com>
2024-03-11 14:37:13 +0100

    Adding str parsing for request
    taking tcp read and parsing. 1/2

--- single_thread_server/src/http/request.rs
+++ single_thread_server/src/http/request.rs
@@ -14,7 +14,7 @@ pub struct Request {
 impl TryFrom<&[u8]> for Request {
     type Error = ParseError;
 
-    //GET /search?name=abc&sort=1 HTTP/1.1
+    //GET /search?name=abc&sort=1 HTTP/1.1\r\n...HEADERS...
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         /*match str::from_utf8(buf) {
             Ok(request) => {},
@@ -23,13 +23,33 @@ impl TryFrom<&[u8]> for Request {
      */
         let request = str::from_utf8(buf).or(Err(ParseError::InvalidEncoding))?; //this does exactly what the match above does, less easy to read but standard.
 
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; //if Some(result) then ok(result) else if none, error(e) VARIABLE SHADOWING HERE.
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;//first we get GET, now we get /search etc aka the path.
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?; //we dont care about the rest as it should be done diff
+        
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+
         
     }
 
 
 }
 
-
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    let mut iter = request.chars();
+    for (i, c) in request.chars().enumerate() {
+        if c == ' ' || c == '\r' {
+            return Some((&request[..i], &request[i+1..])); //+1 is only going one byte forward not one letter so emojis would fail this BUT we know that there is a space, which is one byte, so it skips that only.
+        }
+    }
+    None
+    }
+   
 pub enum ParseError {
     InvalidRequest,
     InvalidEncoding,
